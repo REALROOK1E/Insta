7aef60c2ebd7a7660365fdf477e7251b50d88ea2
REALROOK1E <103976838+REALROOK1E@users.noreply.github.com>
2025-06-20 22:16:26 +0200

    Added Satoken for login and verify

--- Insta-auth/src/main/java/com/zekai/insta/auth/Controller/TestController.java
+++ Insta-auth/src/main/java/com/zekai/insta/auth/Controller/TestController.java
@@ -1,12 +1,14 @@
 package com.zekai.insta.auth.Controller;
 
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.zekai.framework.common.response.Response;
 import com.zekai.insta.auth.domain.dataobject.UserDO;
 import com.zekai.framework.oplog.aspect.ApiOperationLog;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,10 +21,21 @@ public class TestController {
 
     @PostMapping("/test2")
     public Response<User>test2(@RequestBody @Validated User user){
-
         int i=1/0;
         return Response.success(user);
     }
 
-
+    @RequestMapping("/user/doLogin")
+    public String doLogin(String username, String password) {
+        //simulate login
+        if("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+    @RequestMapping("/user/isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
+    }
 }
