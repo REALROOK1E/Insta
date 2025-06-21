75546556c87886edae7168b62afed1b80ce397a0
REALROOK1E <103976838+REALROOK1E@users.noreply.github.com>
2025-06-21 14:14:45 +0200

    服务启动时，把所有角色的权限数据查出来，按角色分组，序列化后存到 Redis

--- Insta-auth/src/main/java/com/zekai/insta/auth/constant/RedisKeyConst.java
+++ Insta-auth/src/main/java/com/zekai/insta/auth/constant/RedisKeyConst.java
@@ -3,12 +3,14 @@ package com.zekai.insta.auth.constant;
 /**
  * @author: ZeKai
  * @date: 2025/6/20
- * @description: 常量类，用于拼接短信
+ * @description: 常量类
  **/
 public class RedisKeyConst {
 
         private static final String VERIFICATION_CODE_KEY_PREFIX = "verification_code:";
         private static final String USER_ROLES_KEY_PREFIX = "user:roles:";
+
+        private static final String ROLE_PERMISSIONS_KEY_PREFIX = "role:permissions:";
         /**
          * 构建验证码 KEY
          * @param phone
@@ -19,8 +21,13 @@ public class RedisKeyConst {
         public static String buildVerificationCodeKey(String phone) {
             return VERIFICATION_CODE_KEY_PREFIX + phone;
         }
-    public static String buildUserRoleKey(String phone) {
+
+         public static String buildUserRoleKey(String phone) {
         return USER_ROLES_KEY_PREFIX + phone;
+    }
+
+        public static String buildRolePermissionsKey(Long roleId) {
+        return ROLE_PERMISSIONS_KEY_PREFIX + roleId;
     }
 
 }
