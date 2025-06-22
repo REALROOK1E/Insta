9b1f4574fab8116107a5b3ff40c0bfff06df1faf
REALROOK1E <103976838+REALROOK1E@users.noreply.github.com>
2025-06-22 18:55:09 +0200

    实现接口鉴权：Implement interface authentication:

---
+++ insta-gateway/src/main/java/com/zekai/insta/gateway/auth/StpInterfaceImpl.java
@@ -0,0 +1,34 @@
+package com.zekai.insta.gateway.auth;
+import cn.dev33.satoken.stp.StpInterface;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author: ZeKai
+ * @date: 2025/6/22
+ * @description:
+ **/
+@Component
+public class StpInterfaceImpl implements StpInterface {
+
+    @Override
+    public List<String> getPermissionList(Object loginId, String loginType) {
+        // 返回此 loginId 拥有的权限列表
+
+        // todo 从 redis 获取
+
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+        // 返回此 loginId 拥有的角色列表
+
+        // todo 从 redis 获取
+
+        return Collections.emptyList();
+    }
+
+}
