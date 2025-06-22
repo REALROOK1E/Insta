33fd768a40d204c2ff89dd5de2a552e47d6ffa66
REALROOK1E <103976838+REALROOK1E@users.noreply.github.com>
2025-06-22 18:37:47 +0200

    Created a gateway and implements basic routing and forwarding, redirecting from port 8000 to port 8080.

---
+++ insta-gateway/src/main/java/com/zekai/insta/gateway/InstaGatewayApp.java
@@ -0,0 +1,16 @@
+package com.zekai.insta.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ZeKai
+ * @date: 2025/6/22
+ * @description:
+ **/
+@SpringBootApplication
+public class InstaGatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(InstaGatewayApp.class, args);
+    }
+}
