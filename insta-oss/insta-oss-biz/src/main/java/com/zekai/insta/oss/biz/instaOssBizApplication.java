3490d07b217fcdee12b7f304d6da926133f823d9
REALROOK1E <103976838+REALROOK1E@users.noreply.github.com>
2025-06-26 22:17:00 +0200

    oss service added

---
+++ insta-oss/insta-oss-biz/src/main/java/com/zekai/insta/oss/biz/instaOssBizApplication.java
@@ -0,0 +1,17 @@
+package com.zekai.insta.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class instaOssBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(instaOssBizApplication.class, args);
+    }
+
+}
