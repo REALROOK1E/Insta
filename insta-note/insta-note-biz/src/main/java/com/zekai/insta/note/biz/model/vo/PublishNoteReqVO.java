a882f0a8f59cdacb401fa1a1b76137e96ddfd822
REALROOK1E <103976838+REALROOK1E@users.noreply.github.com>
2025-07-12 01:30:48 +0200

    createdANoteServiceFramework

---
+++ insta-note/insta-note-biz/src/main/java/com/zekai/insta/note/biz/model/vo/PublishNoteReqVO.java
@@ -0,0 +1,32 @@
+package com.zekai.insta.note.biz.model.vo;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+/**
+ * @author: ZeKai
+ * @date: 2025/7/12
+ * @description:
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PublishNoteReqVO {
+
+    @NotNull(message = "笔记类型不能为空")
+    private Integer type;
+
+    private List<String> imgUris;
+
+    private String videoUri;
+
+    private String title;
+
+    private String content;
+
+    private Long topicId;
+}
