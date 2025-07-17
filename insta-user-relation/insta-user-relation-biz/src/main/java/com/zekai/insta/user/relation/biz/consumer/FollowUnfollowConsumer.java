383a906e0c03bcfeebcffc316dcbc7fc37806dd7
REALROOK1E <103976838+REALROOK1E@users.noreply.github.com>
2025-07-17 02:13:49 +0200

    Use guava token bucket to achieve current limit, with 5000 tokens per second, and configure parameters through Nacos

--- insta-user-relation/insta-user-relation-biz/src/main/java/com/zekai/insta/user/relation/biz/consumer/FollowUnfollowConsumer.java
+++ insta-user-relation/insta-user-relation-biz/src/main/java/com/zekai/insta/user/relation/biz/consumer/FollowUnfollowConsumer.java
@@ -1,15 +1,21 @@
 package com.zekai.insta.user.relation.biz.consumer;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.zekai.insta.user.relation.biz.constant.MQConstants;
+import com.zekai.insta.user.relation.biz.domain.mapper.FansDOMapper;
+import com.zekai.insta.user.relation.biz.domain.mapper.FollowingDOMapper;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 import org.apache.rocketmq.common.message.Message;
+import org.springframework.transaction.support.TransactionTemplate;
+
 /**
  * @author: ZeKai
  * @date: 2025/7/17
- * @description:
+ * @description: 令牌桶
  **/
 @Component
 @RocketMQMessageListener(consumerGroup = "insta_group", // Group 组
@@ -18,8 +24,21 @@ import org.apache.rocketmq.common.message.Message;
 @Slf4j
 public class FollowUnfollowConsumer implements RocketMQListener<Message> {
 
+    @Resource
+    private FollowingDOMapper followingDOMapper;
+    @Resource
+    private FansDOMapper fansDOMapper;
+    @Resource
+    private TransactionTemplate transactionTemplate;
+    @Resource
+    private RateLimiter rateLimiter;
+    // 每秒创建 5000 个令牌
+
     @Override
     public void onMessage(Message message) {
+
+        // 令牌桶限流
+        rateLimiter.acquire();
         // 消息体
         String bodyJsonStr = new String(message.getBody());
         // 标签
