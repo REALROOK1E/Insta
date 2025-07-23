453d069847857fd511f5591276f7636353ab76c2
ZEKAI <18547221242@163.com>
2025-07-23 18:44:02 +0800

    fixed a bug of date to string

--- Insta-framework/Insta-common/src/main/java/com/zekai/framework/common/util/DateUtils.java
+++ Insta-framework/Insta-common/src/main/java/com/zekai/framework/common/util/DateUtils.java
@@ -1,7 +1,6 @@
 package com.zekai.framework.common.util;
 
 import com.zekai.framework.common.constants.DateConstants;
-import com.zekai.framework.common.constants.Timeconsts;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -26,30 +25,30 @@ public class DateUtils {
      * LocalDateTime 转友好的相对时间字符串
      *
      * @param dateTime
-     * @return
+     * @return 友好时间字符串，如“3分钟前”、“昨天 12:30”等
      */
-    public static LocalDateTime formatRelativeTime(LocalDateTime dateTime) {
-        // 当前时间
+    public static String formatRelativeTime(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return "";
+        }
         LocalDateTime now = LocalDateTime.now();
-
-        // 计算与当前时间的差距
         long daysDiff = ChronoUnit.DAYS.between(dateTime, now);
         long hoursDiff = ChronoUnit.HOURS.between(dateTime, now);
         long minutesDiff = ChronoUnit.MINUTES.between(dateTime, now);
 
-        if (daysDiff < 1) {  // 如果是今天
-            if (hoursDiff < 1) {  // 如果是几分钟前
+        if (daysDiff < 1) {  // 今天
+            if (hoursDiff < 1) {
                 return minutesDiff + "分钟前";
-            } else {  // 如果是几小时前
+            } else {
                 return hoursDiff + "小时前";
             }
-        } else if (daysDiff == 1) {  // 如果是昨天
+        } else if (daysDiff == 1) {  // 昨天
             return "昨天 " + dateTime.format(DateConstants.DATE_FORMAT_H_M);
-        } else if (daysDiff < 7) {  // 如果是最近一周
+        } else if (daysDiff < 7) {  // 最近一周
             return daysDiff + "天前";
-        } else if (dateTime.getYear() == now.getYear()) {  // 如果是今年
+        } else if (dateTime.getYear() == now.getYear()) {  // 今年
             return dateTime.format(DateConstants.DATE_FORMAT_M_D);
-        } else {  // 如果是去年或更早
+        } else {  // 去年或更早
             return dateTime.format(DateConstants.DATE_FORMAT_Y_M_D);
         }
     }
@@ -61,6 +60,24 @@ public class DateUtils {
      * @return
      */
     public static String localDateTime2String(LocalDateTime time) {
-        return time.format(Timeconsts.DateConstants.DATE_FORMAT_Y_M_D_H_M_S);
+        LocalDateTime now = LocalDateTime.now();
+        long daysDiff = ChronoUnit.DAYS.between(time, now);
+        long hoursDiff = ChronoUnit.HOURS.between(time, now);
+        long minutesDiff = ChronoUnit.MINUTES.between(time, now);
+        if (daysDiff < 1) {
+            if (hoursDiff < 1) {
+                return minutesDiff + "分钟前";
+            } else {
+                return hoursDiff + "小时前";
+            }
+        } else if (daysDiff == 1) {
+            return "昨天 " + time.format(DateConstants.DATE_FORMAT_H_M);
+        } else if (daysDiff < 7) {
+            return daysDiff + "天前";
+        } else if (time.getYear() == now.getYear()) {
+            return time.format(DateConstants.DATE_FORMAT_M_D);
+        } else {
+            return time.format(DateConstants.DATE_FORMAT_Y_M_D);
+        }
     }
 }
